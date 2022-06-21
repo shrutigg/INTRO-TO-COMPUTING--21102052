ff9a168e692c275ebf99bba8bffe17bab6e2b7cf
shrutigg <99747877+shrutigg@users.noreply.github.com>
2022-06-21 13:19:42 +0530

    Create Ass5 output.py

---
+++ Ass5 output.py
@@ -0,0 +1,103 @@
+#Question 1
+Enter the string: Python is case sensitive
+evitisnes esac si nohtyP
+
+
+
+#Question 2
+Enter the lower limit of the range:1
+Enter the upper limit of the range:30
+Enter the number to divide:5
+5
+10
+15
+20
+25
+
+
+
+#Question 3
+Enter the length of side 1:3
+Enter the length of side 2:4
+Enter the length of side 3:5
+6.0
+
+
+#Question 4
+* 
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+
+#Question 5
+Enter The Number Of Rows: 6
+
+A
+B C
+D E F
+G H I J
+K L M N O
+P Q R S T U
+
+
+
+#Question 6
+Enter the lower limit of the range:1
+Enter the upper limit of the range:60
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+
+
+#Question 7
+77
+154
+231
+308
+385
+462
+
+
+#Question 8
+Enter the list of numbers seperated by comma(,):1,-20,30,51,61,32,-67,52,51,-20
+1 occurs 1  times
+-20 occurs 2  times
+30 occurs 1  times
+51 occurs 2  times
+61 occurs 1  times
+32 occurs 1  times
+-67 occurs 1  times
+52 occurs 1  times
+51 occurs 2  times
+-20 occurs 2  times
+List of Positive Numbers:  [1, 30, 51, 61, 32, 52, 51]
+List of Negative Numbers:  [-20, -67, -20]
+List of Even Numbers:  [-20, 30, 32, 52, -20]
+List of Even Numbers:  [1, 51, 61, -67, 51]
+  
+
+  
+#Question 9
+Enter the list of words seperated by comma(,):my,name,name,is,issac,issac
+{'my': 1, 'name': 2, 'is': 1, 'issac': 2}
